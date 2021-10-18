c9b487576fbf6367d245e63760e7fbebe3a80c9e
edmilsonvaldeciopinton <edmilson.valdecio.pinton@gmail.com>
2021-10-18 19:26:06 -0300

    Testes das classes AutorService e LivroService

---
+++ src/test/java/br/com/alura/livraria/service/LivroServiceTest.java
@@ -0,0 +1,58 @@
+package br.com.alura.livraria.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.alura.livraria.dto.LivroDto;
+import br.com.alura.livraria.dto.LivroFormDto;
+import br.com.alura.livraria.repository.AutorRepository;
+import br.com.alura.livraria.repository.LivroRepository;
+
+@ExtendWith(MockitoExtension.class)
+class LivroServiceTest {
+
+	@Mock
+	private LivroRepository livroRepository;
+
+	@Mock
+	private AutorRepository autorRepository;
+
+	@InjectMocks
+	private LivroService service;
+
+	@Test
+	void deveriaCadastrarUmLivro() {
+		LivroFormDto formLivroDto = new LivroFormDto("E o Vento Levou", LocalDate.now(), 100, 1l);
+
+		LivroDto dto = service.cadastrar(formLivroDto);
+		
+		Mockito.verify(livroRepository.save(Mockito.any()));
+
+		assertEquals(formLivroDto.getTitulo(), dto.getTitulo());
+		assertEquals(formLivroDto.getDataLancamento(), dto.getDataLancamento());
+		assertEquals(formLivroDto.getNumeroPaginas(), dto.getNumeroPaginas());
+
+	}
+
+	@Test
+	void naodeveriaCadastrarUmLivroComAutorInexistente() {
+		LivroFormDto formLivroDto = new LivroFormDto("E o Vento Levou", LocalDate.now(), 100, 1l);
+
+		Mockito.when(autorRepository.getById(formLivroDto.getAutorId())).thenThrow(EntityNotFoundException.class);
+
+		assertThrows(IllegalArgumentException.class, () -> service.cadastrar(formLivroDto));
+
+	}
+
+}
